3d8b34f7fa5f23265a3b86ab86565f7f261cddd8
Sajith Dilshan <sajithdilshan@gmail.com>
2013-03-11 13:50:19 +0530

    fixed search error in product

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,7 +8,7 @@ class Product < ActiveRecord::Base
 
 	def self.search_products(column, search, page, p_page)
 		if search and column
-			paginate :per_page => p_page, :page => page, :conditions => ["#{column} LIKE ?", "%#{search}%"], :order => column
+			paginate :per_page => p_page, :page => page, :conditions => ["CAST(#{column} AS TEXT) LIKE ?", "%#{search}%"], :order => column
 		else
 			return nil
 		end
